6442acc55c8a7911620992a532ee8e5d3e9c53f7
Alex Skorulis <skorulis@gmail.com>
2018-07-19 07:44:44 +1000

    Allow moving a sprite around the map

--- floatios/view controllers/dungeon/DungeonViewController.swift
+++ floatios/view controllers/dungeon/DungeonViewController.swift
@@ -11,6 +11,11 @@ import SpriteKit
 
 class DungeonViewController: UIViewController {
     
+    var map:SKTileMapNode!
+    var tank:SKSpriteNode!
+    var sceneView:SKView!
+    var scene:SKScene!
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         self.title = "Dungeon"
@@ -27,10 +32,35 @@ class DungeonViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = SKScene(fileNamed: "CityScene")
-        let skView = self.view as! SKView
+        self.scene = SKScene(fileNamed: "CityScene")
+        self.sceneView = self.view as! SKView
         
-        skView.presentScene(scene)
+        sceneView.presentScene(scene)
+        
+        tank = SKSpriteNode(imageNamed: "tank")
+        scene?.addChild(tank)
+        
+        map =  scene?.childNode(withName: "map") as! SKTileMapNode
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapped(recognizer:)))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func tapped(recognizer:UITapGestureRecognizer) {
+        if recognizer.state != .ended {
+            return
+        }
+        
+        let viewLoc = recognizer.location(in: recognizer.view)
+        let location = self.scene.convertPoint(fromView: viewLoc)
+        let mapLoc = self.map.convert(location, from: self.scene)
+        
+        let column = map.tileColumnIndex(fromPosition: mapLoc)
+        let row = map.tileRowIndex(fromPosition: mapLoc)
+        
+        let centrePos = map.centerOfTile(atColumn: column, row: row)
+        let action = SKAction.move(to: centrePos, duration: 0.25)
+        action.timingMode = .easeInEaseOut
+        self.tank.run(action)
         
     }
 
