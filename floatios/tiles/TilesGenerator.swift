d882e1b68273d0663f10ebc3fba643ff3106ff51
Alex Skorulis <skorulis@gmail.com>
2018-07-20 18:12:07 +1000

    Generate very simple walls

--- floatios/tiles/TilesGenerator.swift
+++ floatios/tiles/TilesGenerator.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SKSwiftLib
+import SpriteKit
 
 class TilesGenerator: NSObject {
 
@@ -27,9 +28,10 @@ class TilesGenerator: NSObject {
         print("Writing tiles to " + path)
     }
     
-    func saveTile(img:UIImage,name:String) {
-        let data = UIImagePNGRepresentation(img)
-        let fileName = name + ".png"
+    func saveTile(def:SKTileDefinition) {
+        let cgImage = def.textures.first!.cgImage()
+        let data = UIImagePNGRepresentation(UIImage(cgImage: cgImage))
+        let fileName = def.name! + ".png"
         let fileURL = PathHelper.documentsDirectory().appendingPathComponent(rootDir).appendingPathComponent(fileName)
         do {
             try data?.write(to: fileURL)
@@ -37,21 +39,60 @@ class TilesGenerator: NSObject {
         
     }
     
-    func generateEmptyTile(name:String) -> UIImage {
+    func generateEmptyTile() -> UIImage {
         UIGraphicsBeginImageContext(tileSize)
-        let ctx = UIGraphicsGetCurrentContext()
+        let ctx = UIGraphicsGetCurrentContext()!
         
+        ctx.move(to: CGPoint(x: tileSize.width/2, y: 0))
+        ctx.addLine(to: CGPoint(x: tileSize.width, y: tileSize.height/4))
+        ctx.addLine(to: CGPoint(x: tileSize.width, y: tileSize.height * 0.75))
+        ctx.addLine(to: CGPoint(x: tileSize.width/2, y: tileSize.height))
+        ctx.addLine(to: CGPoint(x: 0, y: tileSize.height * 0.75))
+        ctx.addLine(to: CGPoint(x: 0, y: tileSize.height/4))
         
+        ctx.setFillColor(UIColor.brown.cgColor)
+        ctx.fillPath()
         
         let img = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
         return img!
     }
     
+    func generateWallImage(adj:SKTileAdjacencyMask) -> UIImage {
+        UIGraphicsBeginImageContext(tileSize)
+        let ctx = UIGraphicsGetCurrentContext()!
+        
+        ctx.setStrokeColor(UIColor.lightGray.cgColor)
+        ctx.setLineWidth(10)
+        ctx.move(to: CGPoint(x: 0, y: tileSize.height/2))
+        ctx.addLine(to: CGPoint(x:tileSize.width,y:tileSize.height/2))
+        ctx.strokePath()
+        
+        let img = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return img!
+    }
+    
+    func generateTileDefinition(name:String) -> SKTileDefinition {
+        let img = generateEmptyTile()
+        let def = SKTileDefinition(texture: SKTexture(image: img))
+        def.name = name
+        return def
+    }
+    
+    func generateWallDefinition(adj:SKTileAdjacencyMask) -> SKTileDefinition {
+        let img = generateWallImage(adj: adj)
+        let def = SKTileDefinition(texture: SKTexture(image: img))
+        def.name = "wall"
+        return def
+    }
+    
     static func generateAllTiles() {
         let gen = TilesGenerator(tileSize: CGSize(width: 60, height: 70))
-        let t1 = gen.generateEmptyTile(name: "test")
-        gen.saveTile(img: t1, name: "test")
+        let tile = gen.generateTileDefinition(name: "test")
+        let t2 = gen.generateWallDefinition(adj: .adjacencyAll)
+        gen.saveTile(def: tile)
+        gen.saveTile(def: t2)
     }
     
 }
