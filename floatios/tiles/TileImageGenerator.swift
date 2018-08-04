cacfc1ab43dad7445de0b245168ce84c1ea6331e
Alex Skorulis <skorulis@gmail.com>
2018-08-04 16:40:31 +1000

    Move ImageGen out to SKSwiftLib

--- floatios/tiles/TileImageGenerator.swift
+++ floatios/tiles/TileImageGenerator.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SpriteKit
 import FLGame
+import SKSwiftLib
 
 class TileImageGenerator: ImageGen {
     
--- floatios/view controllers/map/HexTextureGenerator.swift
+++ floatios/view controllers/map/HexTextureGenerator.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SKSwiftLib
 
 public class HexTextureGenerator: ImageGen {
 
